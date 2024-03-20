b5b45608c9032e2f8d3d15323086b3040107f688
serhiiderkach <serhiiderkach1995@gmail.com>
2024-03-19 21:52:18 -0500

    update

--- src/pages/Home/components/CoinTable/components/CoinTableBody/types.ts
+++ src/pages/Home/components/CoinTable/components/CoinTableBody/types.ts
@@ -10,13 +10,10 @@ export interface ICoinNumProps {
 
 export interface IPrice1hCol {
     price_change_percentage_1h_in_currency: number,
-}
+};
 
 export interface ICoinPriceProps {
     current_price: number,
-
-    price_change_percentage_7d_in_currency?: number,
-    price_change_percentage_24h?: number,
 };
 
 export type TCoinTableRow = ICoinNameProps & ICoinNumProps & ICoinPriceProps & IPrice1hCol;
